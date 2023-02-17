1e2e483395aaa10e6b72b3de0e1f00d734c3c594
safem0de <n.wangwanich@gmail.com>
2023-02-17 11:58:27 +0700

    use fastAPI

--- _fastAPI/database.py
+++ _fastAPI/database.py
@@ -6,8 +6,15 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///_fastAPI/database.sqlite"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
+
+# https://stackoverflow.com/questions/30314882/using-pysqlcipher-with-sqlalchemy
+# engine = create_engine(
+#     'sqlite+pysqlcipher://:{0}@/{1}?'
+#     'cipher=aes-256-cfb&kdf_iter=64000'.format('testtest', 'database.sqlite'))
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
