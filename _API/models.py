9a4a0ba787f6a037706ea8969fd709bd4b2ba6da
safem0de <n.wangwanich@gmail.com>
2023-02-17 11:02:24 +0700

    use fastAPI is better

--- _API/models.py
+++
@@ -1,6 +0,0 @@
-from _API import db
-from datetime import datetime
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False, unique=True)
--- _Controllers/example.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-URL = "http://127.0.0.1:5000/weather"
-response = requests.get(URL)
-message = response.json()
-print(message["data"])
