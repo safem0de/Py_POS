c3e21b570cee3d7e9df7ad32f20bf8a1b99171ed
safem0de <n.wangwanich@gmail.com>
2023-02-17 11:32:17 +0700

    fastAPI fuking good

--- _fastAPI/main.py
+++ _fastAPI/main.py
@@ -1,32 +1,3 @@
-# from typing import Union
-
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-
-# import uvicorn
-
-# app = FastAPI()
-
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
-
-
-# @app.get("/")
-# async def read_root():
-#     return {"Hello": "Test"}
-
-
-# @app.get("/items/{item_id}")
-# async def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-# @app.put("/items/{item_id}")
-# async def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
-
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -37,7 +8,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -45,6 +15,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Test"}
 
 
 @app.post("/users/", response_model=schemas.User)
