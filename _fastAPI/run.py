ad67a3031496512474464f75b1d178cbd604ceb8
safem0de <n.wangwanich@gmail.com>
2023-03-21 23:07:42 +0700

    update readme prevent forgot command

--- _fastAPI/run.py
+++ _fastAPI/run.py
@@ -1,5 +1,6 @@
 import uvicorn
 from multiprocessing import cpu_count, freeze_support
+import main as m
 
 # # https://medium.com/codex/how-to-create-a-fastapi-uvicorn-server-windows-service-af41f075dabf
 def start_server(host="127.0.0.1",
@@ -16,6 +17,11 @@ def start_server(host="127.0.0.1",
 
 
 if __name__ == "__main__":
+    try:
+        m.create_db_and_tables()
+    except:
+        print("database already created")
+
     freeze_support()  # Needed for pyinstaller for multiprocessing on WindowsOS
     num_workers = int(cpu_count() * 0.75)
     start_server(num_workers=num_workers)
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
 D:\PythonProject\Py_POS\_fastAPI> pyinstaller run.py --onefile --console --clean --hidden-import _fastAPI.main
+
+# migrate with mac
+python3 -m venv .
+(Py_POS) naweeparbwangwanich@Naweeparbs-Air Py_POS % python3 _fastAPI/run.py
